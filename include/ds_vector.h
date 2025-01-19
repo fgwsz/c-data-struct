2f07dbba77298c3dfe0ed6e663e8cb941dc27c3c
fgwsz <3191000458@qq.com>
2025-01-19 11:44:53 +0800

    add api of ds_vector

--- include/ds_vector.h
+++ include/ds_vector.h
@@ -1,5 +1,20 @@
 #ifndef DS_VECTOR_H
 #define DS_VECTOR_H
-struct ds_Vector;
-
+#include<stddef.h>//size_t
+typedef struct Vector{
+    void** data_;
+    size_t capacity_;
+    size_t size_;
+}ds_vector;
+#define DS_VECTOR_DEFAULT_CAPACITY 10
+int ds_vector_insert(ds_vector* self,size_t position,void* value);
+int ds_vector_erase(ds_vector* self,size_t position);
+int ds_vector_set(ds_vector* self,size_t position,void* value);
+void* ds_vector_get(ds_vector* self,size_t position);
+void* ds_vector_foreach(
+    ds_vector* self
+    ,size_t begin
+    ,size_t end
+    ,void*(*function)(void*)
+);
 #endif//DS_VECTOR_H
